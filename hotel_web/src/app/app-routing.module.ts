c5de55e5e9e05dad2441670f95193d17efe27cbc
Sheryar <sheryarstorm1033@gmail.com>
2022-11-01 19:50:35 +0500

    c

--- hotel_web/src/app/app-routing.module.ts
+++ hotel_web/src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { scrollOffset: [0, 0], scrollPositionRestoration: 'top' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
